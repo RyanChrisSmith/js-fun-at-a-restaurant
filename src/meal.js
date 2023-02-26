d22bd2b2953ae65dee83d38b402e4ba2baa2d69c
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-02-26 15:52:06 -0700

    initial meal tests passing / iteration 1

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
+function nameMenuItem(item) {
+  return `Delicious ${item}`;
+}
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  };
+}
+
+function addIngredients(name, ingredientList) {
+  if (!ingredientList.includes(name)) {
+    ingredientList.push(name);
+  }
+}
+
+function formatPrice(price) {
+  return `$${price}`
+}
+
+function decreasePrice(price) {
+  return price - (price * .10)
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  };
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
