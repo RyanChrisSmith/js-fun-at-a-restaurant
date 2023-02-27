48f4c2d9982d8cbb58455ebb981e210441a8c99c
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-02-27 08:58:26 -0700

    order tests passing

--- src/order.js
+++ src/order.js
@@ -1,7 +1,31 @@
+function takeOrder(order, deliveryOrders ) {
+  if (deliveryOrders.length < 3){
+    deliveryOrders.push(order);
+  }
+}
+
+function refundOrder(thisOrderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === thisOrderNumber) {
+      deliveryOrders.splice(i, 1);
+      break;
+    }
+  }
+}
+
+function listItems(deliveryOrders) {
+  return deliveryOrders.map(order => order.item).join(", ");
+}
+
+function searchOrder(deliveryOrders, name) {
+  return deliveryOrders.some(function(order) {
+    return order.item === name;
+  });
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
