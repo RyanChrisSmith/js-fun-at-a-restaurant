c8fce79d5771bcebdce5e4c9668a2511a27ed965
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-02-27 11:44:01 -0700

    Chef tests complete, checkForFood function is a doozy

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,27 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+
+  greetCustomer(patron, isMorning) {
+    if (isMorning) {
+      return `Good morning, ${patron}!`;
+    } else {
+      return `Hello, ${patron}!`
+    }
+  }
+
+  checkForFood(foodItem) {
+    const menus = this.restaurant.menus;
+    for (const type in menus) {
+      const itemIndex = menus[type].findIndex(item => item.type === type);
+      if (itemIndex !== -1) {
+        menus[type].splice(itemIndex, 1);
+        return `Yes, we're serving ${foodItem.name} today!`
+      }
+    }
+    return `Sorry, we aren't serving ${foodItem.name} today.`
   }
 }
 
