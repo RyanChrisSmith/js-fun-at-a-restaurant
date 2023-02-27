31c171caeee6b74a625dcd58b3e9da7332f77fb4
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-02-27 11:22:33 -0700

    menu tests passing, struggled with removing menu items but got it working elegantly

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,35 @@
+function createRestaurant(name) {
+  return {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+}
 
+function addMenuItem(restaurant, item) {
+  if (!restaurant.menus[item.type].includes(item)) {
+    restaurant.menus[item.type].push(item);
+  }
+}
+
+function removeMenuItem(restaurant, name, type) {
+  var itemIndex = restaurant.menus[type].findIndex(function(item) {
+    return item.name === name;
+  });
+
+  if (itemIndex !== -1) {
+    restaurant.menus[type].splice(itemIndex, 1);
+    return `No one is eating our ${name} - it has been removed from the ${type} menu!`;
+  } else {
+    return `Sorry, we don't sell ${name}, try adding a new recipe!`
+  }
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
